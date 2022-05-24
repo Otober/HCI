01af31558936f22be736065240e75e8a9198c87a
October <jjehdud@naver.com>
2022-05-24 10:42:02 +0900

    solved error

--- test3.py
+++ test3.py
@@ -181,27 +181,16 @@ POSE_PAIRS_BODY_25 = [[0, 1], [0, 15], [0, 16], [1, 2], [1, 5], [1, 8], [8, 9], 
                           15, 17], [16, 18], [14, 21], [19, 21], [20, 21],
                       [11, 24], [22, 24], [23, 24]]
 
-if platform.system() == "Linux" :
-    # 신경 네트워크의 구조를 지정하는 prototxt 파일 (다양한 계층이 배열되는 방법 등)
-    protoFile_mpi = "HCI\\models\\pose\\mpi\\pose_deploy_linevec.prototxt"
-    protoFile_mpi_faster = "HCI\\models\\pose\\mpi\\pose_deploy_linevec_faster_4_stages.prototxt"
-    protoFile_coco = "HCI\\models\\pose\\coco\\pose_deploy_linevec.prototxt"
-    protoFile_body_25 = "HCI\\models\\pose\\body_25\\pose_deploy.prototxt"
+# 신경 네트워크의 구조를 지정하는 prototxt 파일 (다양한 계층이 배열되는 방법 등)
+protoFile_mpi = "HCI\\models\\pose\\mpi\\pose_deploy_linevec.prototxt"
+protoFile_mpi_faster = "HCI\\models\\pose\\mpi\\pose_deploy_linevec_faster_4_stages.prototxt"
+protoFile_coco = "HCI\\models\\pose\\coco\\pose_deploy_linevec.prototxt"
+protoFile_body_25 = "HCI\\models\\pose\\body_25\\pose_deploy.prototxt"
 
-    # 훈련된 모델의 weight 를 저장하는 caffemodel 파일
-    weightsFile_mpi = "HCI\\models\\pose\\mpi\\pose_iter_160000.caffemodel"
-    weightsFile_coco = "HCI\\models\\pose\\coco\\pose_iter_440000.caffemodel"
-    weightsFile_body_25 = "HCI\\models\\pose\\body_25\\pose_iter_584000.caffemodel"
-
-elif platform.system() == "Windows" :
-    protoFile_mpi = "HCI\\models\\pose\\mpi\\pose_deploy_linevec.prototxt"
-    protoFile_mpi_faster = "HCI\\models\\pose\\mpi\\pose_deploy_linevec_faster_4_stages.prototxt"
-    protoFile_coco = "HCI\\models\\pose\\coco\\pose_deploy_linevec.prototxt"
-    protoFile_body_25 = "HCI\\models\\pose\\body_25\\pose_deploy.prototxt"
-
-    weightsFile_mpi = "HCI\\models\\pose\\mpi\\pose_iter_160000.caffemodel"
-    weightsFile_coco = "HCI\\models\\pose\\coco\\pose_iter_440000.caffemodel"
-    weightsFile_body_25 = "HCI\\models\\pose\\body_25\\pose_iter_584000.caffemodel"
+# 훈련된 모델의 weight 를 저장하는 caffemodel 파일
+weightsFile_mpi = "HCI\\models\\pose\\mpi\\pose_iter_160000.caffemodel"
+weightsFile_coco = "HCI\\models\\pose\\coco\\pose_iter_440000.caffemodel"
+weightsFile_body_25 = "HCI\\models\\pose\\body_25\\pose_iter_584000.caffemodel"
 
 # 키포인트를 저장할 빈 리스트
 points = []
