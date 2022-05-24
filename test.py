a19ff92d06b54d216dcc84556af489ac5196ab37
Otober <jjehdud@naver.com>
2022-05-24 19:14:16 +0900

    add platform

--- test.py
+++ test.py
@@ -1,15 +1,5 @@
-import cv2
+import platform
 
-cap = cv2.VideoCapture(0)
-cnt = 0
-
-while(True):
-    ret, frame = cap.read()    # Read 결과와 frame
-    cnt = cnt + 1
-    print(cnt)
-    if(ret) :
-        cv2.imshow('frame_color', frame)    # 컬러 화면 출력
-        if cv2.waitKey(1) == ord('q'):
-            break
-cap.release()
-cv2.destroyAllWindows()
+print()
+if platform.system() == "Linux" :
+    print("Linux")
